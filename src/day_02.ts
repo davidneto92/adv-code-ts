e9f93e1a1c29230aa888deb69a4cfd9caf31bea1
David Neto <davidneto92@gmail.com>
2024-08-29 16:22:14 -0400

    finish day 2 part 2

--- src/day_02.ts
+++ src/day_02.ts
@@ -72,11 +72,55 @@ const parseGameData = (gameData: string): number | 0 => {
   return validGameId
 }
 
+const parseGameDataPart2 = (gameData: string): number | 0 => {
+  let validGameId = getFirstNumber(gameData)
+  let games: string[]
+
+  games = gameData.split(':')
+  if (games[1]?.length) {
+    games = games[1].split(';')
+  } else {
+    throw new Error('no game data present')
+  }
+
+  let maxColorValues: TParsedGameData = { red: 0, blue: 0, green: 0 }
+  // iterates through all rounds
+  for (let gameIndex = 0; gameIndex < games.length; gameIndex++) {
+    const game = games[gameIndex];
+    if (!game) {
+      throw new Error('invalid game data')
+    }
+
+    const diceCounts = game.split(',')
+    // iterates through color results in a round
+    for (let diceIndex = 0; diceIndex < diceCounts.length; diceIndex++) {
+      const diceResult = diceCounts[diceIndex] || '';
+      const count = getFirstNumber(diceResult)
+      const color = getDiceColor(diceResult) as TColor
+
+      if (!color.length) {
+        const errorStr = `invalid color detected within Game ${validGameId}`
+        throw new Error(errorStr)
+      }
+
+      maxColorValues[color] = Math.max(maxColorValues[color], count)
+    }
+  }
+
+  const powers = Object
+    .values(maxColorValues)
+    .reduce((acc: number, current: number) => acc * current, 1)
+  return powers
+}
+
 const stream = getFileStream('src/input/day_02.txt')
 let idSum = 0
+let powerSum = 0
 stream
-  .on('line', gameData => {
-    const validId = parseGameData(gameData)
-    idSum += validId
+  .on('line', (gameData: string) => {
+    // part 1
+    // const validId = parseGameData(gameData)
+    // part 2
+    powerSum += parseGameDataPart2(gameData)
   })
-  .on('close', () => console.log({ idSum }))
+  .on('close', () => console.log({ powerSum }))
